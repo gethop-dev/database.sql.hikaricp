e9fb05c4b43bd1732e24cb31863cae5005397cb1
Bingen Galartza Iparragirre <bingen.galartza@magnet.coop>
2022-11-28 11:15:14 +0100

    Set project deployment options to Clojars

--- project.clj
+++ project.clj
@@ -10,6 +10,14 @@
                  [hikari-cp "2.13.0"]
                  [integrant "0.8.0"]
                  [net.ttddyy/datasource-proxy "1.7"]]
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+                                      :username :env/CLOJARS_USERNAME
+                                      :password :env/CLOJARS_PASSWORD
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://clojars.org/repo"
+                                      :username :env/CLOJARS_USERNAME
+                                      :password :env/CLOJARS_PASSWORD
+                                      :sign-releases false}]]
   :profiles
   {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.36.0.3"]
                         [org.slf4j/slf4j-nop "1.7.32"]
