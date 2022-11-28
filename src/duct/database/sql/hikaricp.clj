e7f3015859fe36e5b4a9334441c041838adec6d0
Bingen Galartza Iparragirre <bingen.galartza@magnet.coop>
2022-11-28 10:54:26 +0100

    Make jdbc-url and connection-uri optional
    Details in Issue 10 of the original repository [1]
    
    [1] https://github.com/duct-framework/database.sql.hikaricp/issues/10

--- src/duct/database/sql/hikaricp.clj
+++ src/duct/database/sql/hikaricp.clj
@@ -41,7 +41,8 @@
   [_ {:keys [logger connection-uri jdbc-url] :as options}]
   (sql/->Boundary {:datasource
                    (-> (dissoc options :logger)
-                       (assoc :jdbc-url (or jdbc-url connection-uri))
+                       (cond-> (and (not jdbc-url) connection-uri)
+                         (assoc :jdbc-url connection-uri))
                        (hikari-cp/make-datasource)
                        (cond-> logger (wrap-logger logger)))}))
 
